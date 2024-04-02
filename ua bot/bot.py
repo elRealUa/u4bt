ba312526826df4929903a15854d5b8c8366a972c
elRealUa <161535328+elRealUa@users.noreply.github.com>
2024-04-01 19:50:52 -0500

    Add files via upload

---
+++ ua bot/bot.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+import os
+import random
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+TOKEN = ""
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def mem(ctx):
+    image_name = random.choice(os.listdir("images"))
+    with open(f"images/{image_name}", "rb") as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('dog')
+async def dog(ctx):
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
+
+bot.run(TOKEN)
